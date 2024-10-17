c04b9a56fb8d820bc38ddda521c9158daa4bbca6
rafiazharr17 <rafipilo17@gmail.com>
2024-10-17 21:43:21 +0700

    menambahkan Composable dan fungsi baru

--- app/src/main/java/com/rafi/myapplication/MainActivity.kt
+++ app/src/main/java/com/rafi/myapplication/MainActivity.kt
@@ -43,6 +43,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun basicComposable(){
+
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
