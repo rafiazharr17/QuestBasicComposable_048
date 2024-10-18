ae4a7870d724fb2a5814f9c775e386aab8e24761
rafiazharr17 <rafipilo17@gmail.com>
2024-10-19 01:35:10 +0700

    mengganti gambar, lalu mengubah bentuk gambar menjadi bulat

--- app/src/main/java/com/rafi/myapplication/MainActivity.kt
+++ app/src/main/java/com/rafi/myapplication/MainActivity.kt
@@ -10,12 +10,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
@@ -94,9 +100,11 @@ fun basicComposable(rafi: Modifier = Modifier){
         )
 
         Image(
-            painter = painterResource(id = R.drawable.barca),
-            contentDescription = "Barcelona",
-            modifier = Modifier.size(300.dp)
+            painter = painterResource(id = R.drawable.potorapi),
+            contentDescription = "poto",
+            modifier = Modifier.size(400.dp)
+                .clip(CircleShape),
+            contentScale = ContentScale.Crop
         )
     }
 }
